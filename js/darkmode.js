a1283815d47ee3e12b93ca97c351d80c169e83d1
淡捌曇 / Awayatan <133568188+Awayatan@users.noreply.github.com>
2023-05-25 13:12:36 +0900

    Add files via upload

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,3 +1,5 @@
+/* 2023/05/25 update */
+
 const btn = document.querySelector("#darkmode");
 const root = document.documentElement;
 
@@ -10,24 +12,31 @@ btn.addEventListener("change", () => {
 		root.style.setProperty("--boxPinkColour","#8f8684");
 		root.style.setProperty("--boxGreenColour","#8e8f84");
 		root.style.setProperty("--boxBlueColour","#84868f");
+		root.style.setProperty("--lineOrange","#ffd48da0");
+		root.style.setProperty("--lineLightblue","#e4f7f3a0");
 		root.style.setProperty("--fontColourMain","#e3e3e3");
+		root.style.setProperty("--fontColourLinks","#9bdee8");
 		root.style.setProperty("--fontColourIndex","#e3e3e3");
-		root.style.setProperty("--fontColourSmallindex","#1e1e1e");
+		root.style.setProperty("--fontColourSmallindex","#cecece");
 		root.style.setProperty("--titleareaColour","linear-gradient(90deg, #8f6e5a, 70%, #915895)");
 		root.style.setProperty("--articleTitleColour","#9f6e5a");
+		root.style.setProperty("--opacityRate","0.3");
   } else {
 		root.style.setProperty("--backgroundColour", "#fff6f4");
 		root.style.setProperty("--quoteBoxColour", "#fbf2ee");
-		root.style.setProperty("--fixedBoxColour", "linear-gradient(210deg, #d4a4a4, 70%, #9198e5);");
+		root.style.setProperty("--fixedBoxColour", "linear-gradient(210deg, #d4a4a4, 70%, #9198e5)");
 		root.style.setProperty("--lineColour","#d3d3d3");
 		root.style.setProperty("--boxPinkColour","#efe6e4");
 		root.style.setProperty("--boxGreenColour","#eeefe4");
 		root.style.setProperty("--boxBlueColour","#e4e6ef");
+		root.style.setProperty("--lineOrange","#f0cc6890");
+		root.style.setProperty("--lineLightblue","#c0e3ec90");
 		root.style.setProperty("--fontColourMain","#111111");
+		root.style.setProperty("--fontColourLinks","#33acb2");
 		root.style.setProperty("--fontColourIndex","#343434");
 		root.style.setProperty("--fontColourSmallindex","#717171");
 		root.style.setProperty("--titleareaColour","linear-gradient(90deg, #cfae9a, 70%, #d198d5)");
 		root.style.setProperty("--articleTitleColour","#cfae9a");
-
+		root.style.setProperty("--opacityRate","0");
 	}
 });
---
+++ js/fixedbox.js
@@ -0,0 +1,10 @@
+/* 2023/05/24 update */
+
+var fixedBox = document.getElementById('fixedBox');
+var subTitle = document.getElementById('subTitle');
+var verticalTitle = document.getElementById('verticalTitle');
+window.addEventListener('scroll', function() {
+	fixedBox.style.background = (window.scrollY > 50) ? '0' : 'var(--fixedBoxColour)';
+	subTitle.style.color = (window.scrollY > 50) ? '#000000' : '#ffffff';
+	verticalTitle.style.color = (window.scrollY > 50) ? '#000000' : '#ffffff';
+});
